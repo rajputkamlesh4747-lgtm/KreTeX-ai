68225903cc5f0344ef792621b828695b3be13ed9
rajputkamlesh4747-lgtm <rajputkamlesh4747@gmail.com>
2026-08-18 00:12:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,11 @@ if (form) {
     });
   });
 }
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    const button = document.querySelector(".menu-toggle");
+
+    menu.classList.toggle("active");
+    button.classList.toggle("active");
+}
